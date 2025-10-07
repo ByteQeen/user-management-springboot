a5e7b89400f08ae835c76abd6ad1892bab3a3936
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-07 15:29:28 +0300

    add RefreshTokenNotFoundException exception

--- src/main/java/com/sistem_bank/fibank/exceptions/GlobalHandlerException.java
+++ src/main/java/com/sistem_bank/fibank/exceptions/GlobalHandlerException.java
@@ -4,7 +4,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
@@ -30,4 +29,36 @@ public class GlobalHandlerException {
         errorMap.put ("message", roleNotFoundException.getMessage ());
         return new ResponseEntity<> (errorMap, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> handleUserNotFoundException(UserNotFoundException userNotFoundException){
+        Map<String, Object> errorMap = new HashMap<> ();
+        errorMap.put ("timestamp", LocalDateTime.now ());
+        errorMap.put ("status", HttpStatus.NOT_FOUND.value ());
+        errorMap.put ("error", "Not found");
+        errorMap.put ("message", userNotFoundException.getMessage ());
+        return new ResponseEntity<> (errorMap, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<?> handlePasswordMismatchException(PasswordMismatchException passwordMismatchException) {
+        Map<String, Object> errorMap = new HashMap<>();
+        errorMap.put("timestamp", LocalDateTime.now());
+        errorMap.put("status", HttpStatus.BAD_REQUEST.value());
+        errorMap.put("error", "Bad Request");
+        errorMap.put("message", passwordMismatchException.getMessage());
+        return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(RefreshTokenNotFoundException.class)
+    public ResponseEntity<?> handleRefreshTokenNotFoundException(RefreshTokenNotFoundException refreshTokenNotFoundException){
+        Map<String, Object> errorMap = new HashMap<> ();
+        errorMap.put ("timestamp", LocalDateTime.now ());
+        errorMap.put ("status", HttpStatus.NOT_FOUND.value ());
+        errorMap.put ("error", "Not found");
+        errorMap.put ("message", refreshTokenNotFoundException.getMessage ());
+        return new ResponseEntity<> (errorMap, HttpStatus.NOT_FOUND);
+    }
+
 }
+

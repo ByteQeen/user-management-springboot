414c40a32eda778089574e90314664f490afd521
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-07 15:56:20 +0300

    add RefreshToken Entity

--- src/main/java/com/sistem_bank/fibank/domain/RefreshToken.java
+++ src/main/java/com/sistem_bank/fibank/domain/RefreshToken.java
@@ -1,4 +1,35 @@
 package com.sistem_bank.fibank.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class RefreshToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Column(nullable = false, unique = true)
+    private String jti;
+
+    private LocalDateTime expirationDate;
+    private boolean revoked = false;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public RefreshToken() {}
+
 }

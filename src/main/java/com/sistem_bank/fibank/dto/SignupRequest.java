31414c9203345260a935e5e917b59ca4f3784bf4
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-07 14:56:15 +0300

    modify existing SignupRequest DTO

--- src/main/java/com/sistem_bank/fibank/dto/SignupRequest.java
+++ src/main/java/com/sistem_bank/fibank/dto/SignupRequest.java
@@ -1,12 +1,33 @@
 package com.sistem_bank.fibank.dto;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 
-@Data
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
+@Data
 public class SignupRequest {
-    private String username;
-    private String password;
+    @NotNull(message = "Full name cannot be null")
+    @NotEmpty(message = "Full name is required")
+    @Size(min = 3, max = 100, message = "Full name must be between 3 and 100 characters")
+    private String fullName;
+
+
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    @NotNull(message = "Confirm password cannot be null")
+    @Size(min = 8, message = "Confirm password must be at least 8 characters")
+    private String confirmPassword;
+    private boolean termsAccepted;
+    private boolean privacyAccepted;
 }
+

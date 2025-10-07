863f769e2f9f79186436341b02ffce6639ec13b0
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-07 15:04:00 +0300

    modify LoginResponse DTO's fields

--- src/main/java/com/sistem_bank/fibank/dto/LoginResponse.java
+++ src/main/java/com/sistem_bank/fibank/dto/LoginResponse.java
@@ -1,8 +1,8 @@
 package com.sistem_bank.fibank.dto;
 
 public class LoginResponse {
-    private Long id;
-    private String username;
-    private String email;
-    private String token;
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long userId;
 }

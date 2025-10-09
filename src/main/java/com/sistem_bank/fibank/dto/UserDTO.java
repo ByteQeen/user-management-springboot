386f9af1f578e8262fcc383299bb86227e156c78
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-09 12:57:00 +0300

    Add createdAt field to UserDTO

--- src/main/java/com/sistem_bank/fibank/dto/UserDTO.java
+++ src/main/java/com/sistem_bank/fibank/dto/UserDTO.java
@@ -2,9 +2,13 @@ package com.sistem_bank.fibank.dto;
 
 import lombok.Data;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+
 @Data
 public class UserDTO {
     private Long id;
     private String username;
     private String email;
+    private LocalDateTime createdAt;
 }

deed066801bc29f4f130115fabb60c1e9f119b52
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-10 16:05:26 +0300

    feat:add InvalidRefreshTokenException extends

--- src/main/java/com/sistem_bank/fibank/exceptions/InvalidRefreshTokenException.java
+++ src/main/java/com/sistem_bank/fibank/exceptions/InvalidRefreshTokenException.java
@@ -1,4 +1,7 @@
 package com.sistem_bank.fibank.exceptions;
 
-public class InvalidRefreshTokenException {
+public class InvalidRefreshTokenException extends RuntimeException {
+    public InvalidRefreshTokenException(String message){
+        super(message);
+    }
 }

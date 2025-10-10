512d180e73488489a270d8ac57e0926868b83728
Alina-Soltanici <soltanicialina777@gmail.com>
2025-10-10 16:08:19 +0300

    feat:add refresh() method in AuthService

--- src/main/java/com/sistem_bank/fibank/service/AuthService.java
+++ src/main/java/com/sistem_bank/fibank/service/AuthService.java
@@ -1,10 +1,10 @@
 package com.sistem_bank.fibank.service;
 
 import com.sistem_bank.fibank.dto.*;
-import java.util.List;
 
 public interface AuthService {
     SignupResponse signUp(SignupRequest signupRequest);
     LoginResponse login(LoginRequest loginRequest);
+    RefreshTokenResponse refresh(RefreshTokenRequest refreshTokenRequest);
 }
 
